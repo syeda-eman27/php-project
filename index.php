4fea1596abc264c4d64900e74e65e89f9ba463ab
syeda-eman27 <eman2409e@aptechsite.net>
2025-05-26 18:04:22 +0500

    add

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+include 'dbconfig.php';
+if(isset($_POST['btnSave'])){
+    $Mobname = $_POST['mobname'];
+    $MobPrice =$_POST['mobprice'];
+    $MobQunatity = $_POST['mobQunat'];
+    $Mobdesc = $_POST['mobdesc'];
+    $mobimgName = $_FILES['mobimg']['name'];
+    $mobtempName = $_FILES['mobimg']['tmp_name'];
+    $mobimgSize = $_FILES['mobimg']['size'];
+    $TargetDirectry = "./img/";
+    $imgPath = $TargetDirectry.$mobimgName;
+    move_uploaded_file($mobtempName,$imgPath);
+    $sql = "INSERT INTO mobiles (id,MobileName,MobilePrice,MobileQuantity,MobileDesc,MonImgPath) VALUES (Null,'$Mobname','$MobPrice','$MobQunatity','$Mobdesc','$imgPath')";
+    $Query = mysqli_query($conn,$sql);
+}
+?>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-lg-6">
+                <div class="card shadow">
+                    <div class="card-header bg-primary text-white">
+                        <h2 class="mb-0">Add Mobile Phone</h2>
+                    </div>
+                    <div class="card-body">
+                        <form method="post" enctype="multipart/form-data">
+                            <div class="mb-3">
+                                <label for="mobname" class="form-label">Mobile Name:</label>
+                                <input type="text" class="form-control" id="mobname" name="mobname" required>
+                            </div>
+                            
+                            <div class="mb-3">
+                                <label for="mobprice" class="form-label">Price:</label>
+                                <input type="number" class="form-control" id="mobprice" name="mobprice" required>
+                            </div>
+                            
+                            <div class="mb-3">
+                                <label for="mobQunat" class="form-label">Quantity:</label>
+                                <input type="number" class="form-control" id="mobQunat" name="mobQunat" required>
+                            </div>
+                            
+                            <div class="mb-3">
+                                <label for="mobdesc" class="form-label">Description:</label>
+                                <textarea class="form-control" id="mobdesc" name="mobdesc" rows="3" required></textarea>
+                            </div>
+                            
+                            <div class="mb-3">
+                                <label for="mobimg" class="form-label">Image:</label>
+                                <input class="form-control" type="file" id="mobimg" name="mobimg" required>
+                            </div>
+                            
+                            <div class="d-grid gap-2">
+                                <button type="submit" class="btn btn-primary" name="btnSave">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
